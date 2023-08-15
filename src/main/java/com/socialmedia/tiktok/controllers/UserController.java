6e9e4acaf9bdf81895dfe14884b32c1ff40ad330
Ben Hsieh <bqh5026@gmail.com>
2023-08-14 19:33:15 -0500

    Refactor user controller and add user-list.jsp

--- src/main/java/com/socialmedia/tiktok/controllers/UserController.java
+++ src/main/java/com/socialmedia/tiktok/controllers/UserController.java
@@ -1,5 +1,14 @@
 package com.socialmedia.tiktok.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+	
+	@GetMapping("/showUser")
+	public String showUserList() {
+		return "user-list";
+	}
 
 }
