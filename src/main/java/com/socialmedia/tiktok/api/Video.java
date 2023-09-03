0d88b349e82e39184f443e0d66d2ad4ef5a8aada
Ben Hsieh <bqh5026@gmail.com>
2023-09-02 20:22:04 -0500

    Edit video pojo

--- src/main/java/com/socialmedia/tiktok/api/Video.java
+++ src/main/java/com/socialmedia/tiktok/api/Video.java
@@ -1,5 +1,37 @@
 package com.socialmedia.tiktok.api;
 
 public class Video {
+	
+	private int id; 
+	private String title;
+	private String username;
+	private String country;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	} 
+	
+	
 
 }
