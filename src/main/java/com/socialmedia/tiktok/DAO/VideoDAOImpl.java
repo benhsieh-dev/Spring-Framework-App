e1c51be133ae87088b2a97864370b72dea4ffd14
Ben Hsieh <bqh5026@gmail.com>
2023-09-04 21:55:22 -0500

    Add video dao impl

---
+++ src/main/java/com/socialmedia/tiktok/DAO/VideoDAOImpl.java
@@ -0,0 +1,14 @@
+package com.socialmedia.tiktok.DAO;
+
+import java.util.List;
+
+import com.socialmedia.tiktok.api.Video;
+
+public class VideoDAOImpl implements VideoDAO {
+
+	public List<Video> loadVideos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
