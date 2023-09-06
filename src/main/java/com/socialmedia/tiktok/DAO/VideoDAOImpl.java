757762f40cb6b17dc8d34afb920f5d5326299117
Ben Hsieh <bqh5026@gmail.com>
2023-09-05 20:46:49 -0500

    Update VideoController and VideoDAOImpl

--- src/main/java/com/socialmedia/tiktok/DAO/VideoDAOImpl.java
+++ src/main/java/com/socialmedia/tiktok/DAO/VideoDAOImpl.java
@@ -2,10 +2,14 @@ package com.socialmedia.tiktok.DAO;
 
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import com.socialmedia.tiktok.api.Video;
 
+@Repository
 public class VideoDAOImpl implements VideoDAO {
 
+	@Override
 	public List<Video> loadVideos() {
 		// TODO Auto-generated method stub
 		return null;
--- src/main/java/com/socialmedia/tiktok/controllers/VideoController.java
+++ src/main/java/com/socialmedia/tiktok/controllers/VideoController.java
@@ -1,13 +1,23 @@
 package com.socialmedia.tiktok.controllers;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.socialmedia.tiktok.DAO.VideoDAO;
+import com.socialmedia.tiktok.DAO.VideoDAOImpl;
+import com.socialmedia.tiktok.api.Video;
 
 @Controller
 public class VideoController {
 	
+	VideoDAO videoDAO = new VideoDAOImpl(); 
+			
 	@GetMapping("/showVideo")
 	public String showVideoList() {
+		
+		List<Video> loadVideos = videoDAO.loadVideos(); 
 		return "video-list";
 	}
 
