40f7696b0816a660e76c4aa72861dc57a5a55be1
Ben Hsieh <bqh5026@gmail.com>
2023-09-09 23:31:32 -0500

    Edit video dao implementation

--- src/main/java/com/socialmedia/tiktok/DAO/VideoDAOImpl.java
+++ src/main/java/com/socialmedia/tiktok/DAO/VideoDAOImpl.java
@@ -3,16 +3,23 @@ package com.socialmedia.tiktok.DAO;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.socialmedia.tiktok.api.Video;
 
 @Repository
 public class VideoDAOImpl implements VideoDAO {
+	
+	JdbcTemplate jdbcTemplate; 
 
 	@Override
 	public List<Video> loadVideos() {
 		List<Video> videoList = new ArrayList<Video>(); 
+		
+		String sql = "SELECT * FROM videos"; 
+		
+		jdbcTemplate.query(sql, null)
 		return videoList;
 	}
 
