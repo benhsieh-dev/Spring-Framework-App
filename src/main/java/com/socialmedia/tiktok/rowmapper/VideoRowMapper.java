4bd9744c5500f7d8378740140843ed40ee541cd3
Ben Hsieh <bqh5026@gmail.com>
2023-09-10 23:01:08 -0500

    Add video rowmapper

---
+++ src/main/java/com/socialmedia/tiktok/rowmapper/VideoRowMapper.java
@@ -0,0 +1,9 @@
+package com.socialmedia.tiktok.rowmapper;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.socialmedia.tiktok.api.Video;
+
+public class VideoRowMapper implements RowMapper<Video> {
+
+}
